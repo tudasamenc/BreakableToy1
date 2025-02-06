8c76c70454279c71518e6fe9e6f41089b668f3b2
roberto.tuda <roberto.tuda@encora.com>
2025-02-06 17:38:25 -0600

    ToDoClassTest

--- src/main/java/com/example/demo/BreakableToy1Application.java
+++ src/main/java/com/example/demo/BreakableToy1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BreakableToy1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BreakableToy1Application.class, args);
+		LinkedList<ToDo> tasks = new LinkedList<>();
 	}
 
 }
