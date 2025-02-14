4bf53b9c574b3e197112b1499b731c4ac848669a
roberto.tuda <roberto.tuda@encora.com>
2025-02-14 11:27:15 -0600

    Merge remote-tracking branch 'BT1/Backend' into Backend
    # Conflicts:
    #	src/main/java/BreakableToy/Task/TskRepo.java
    #	src/main/java/ToDo.java
    #	src/main/java/com/example/demo/Task/TskController.java

--- src/main/java/BreakableToy/Task/TaskRepository.java
+++ src/main/java/BreakableToy/Task/TaskRepository.java
@@ -40,7 +40,17 @@ public class TaskRepository {
 
     @PostConstruct
     private void init(){
-        tasks.add(new TaskClass(1,"Uno",false,1, LocalDateTime.now(),LocalDateTime.now()));
-        tasks.add(new TaskClass(2,"dos",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(1,"Tarea Uno",false,1, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(2,"Tarea Dos",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(3,"Tarea Tres",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(4,"Tarea Cuatro",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(5,"Tarea Cinco",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(6,"Tarea Seis",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(7,"Tarea Siete",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(8,"Tarea Ocho",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(9,"Tarea Nueve",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(10,"Tarea Diez",false,2, LocalDateTime.now(),LocalDateTime.now()));
+        tasks.add(new TaskClass(11,"Tarea Once",false,2, LocalDateTime.now(),LocalDateTime.now()));
+
     }
 }
